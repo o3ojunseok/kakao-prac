cffd0ed868a28fdfbdc7264442619dc1b8db99ac
o3ojunseok <o3ojunseok@gmail.com>
2023-09-24 02:34:28 +0900

    kakao

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <a @click="loginWithKakao">
+    <img src="/kakao_login_medium_narrow.png"/>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  methods: {
+    kakaoInit() {
+      Kakao.init('3802a5e2aca8230cb8cf145c47efc1bb')
+      Kakao.isInitialized()
+    },
+
+    async loginWithKakao() {
+      await Kakao.Auth.authorize({
+        redirectUri: `${window.location.origin}/kakao-callback`
+      })
+    }
+  },
+  mounted() {
+    this.kakaoInit()
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/kakao-callback.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    로그인 중 입니다...
+  </div>
+</template>
+
+<script>
+export default {
+  async mounted () {
+    try {
+      // code 유무 체크
+      if (!this.$route.query.code) {
+        return this.$router.push('/')
+      }
+      console.log(`code : ${this.$route.query.code}`)
+
+      // 카카오 로그인 요청
+      const body = {
+        code: this.$route.query.code,
+        domain: window.location.origin
+      }
+      console.log(1, body)
+      const response = await this.$axios.$post(
+        '/user/login', body, {}
+      )
+      console.log(response)
+      return response
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+</script>
